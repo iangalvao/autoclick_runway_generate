3d5d2e644ddeca6954880e5a0129d67b5f0ca6a4
iangalvao <iansgalvao@gmail.com>
2025-01-23 00:21:01 -0300

    esc key exit

--- autoclick.py
+++ autoclick.py
@@ -1,5 +1,7 @@
+import msvcrt
 import pyautogui
 import time
+import sys
 
 # Optional: a small sleep to give you time to switch to the correct window
 time.sleep(3)
@@ -12,13 +14,25 @@ button_image_path = 'generate_button.png'
 # PyAutoGUI may require the pillow library or an updated version if it complains about confidence.
 location = pyautogui.locateOnScreen(button_image_path, confidence=0.8)
 
-if location is not None:
-    # Get the center of the location
-    center_x, center_y = pyautogui.center(location)
-    # Move the mouse to the center
-    pyautogui.moveTo(center_x, center_y, duration=0.5)  # 0.5 seconds to move
-    # Click
-    pyautogui.click()
-    print("Generate button clicked!")
-else:
-    print("Generate button not found on screen.")
+while 1:
+    # Check if a key was hit
+    if msvcrt.kbhit():
+        # Get the key pressed
+        key = msvcrt.getch()
+
+        # ESC is b'\x1b'
+        if key == b'\x1b':
+            print("ESC pressed. Exiting...")
+            sys.exit(0)
+
+    if location is not None:
+        # Get the center of the location
+        center_x, center_y = pyautogui.center(location)
+        # Move the mouse to the center
+        pyautogui.moveTo(center_x, center_y, duration=0.5)  # 0.5 seconds to move
+        # Click
+        pyautogui.click()
+        print("Generate button clicked!")
+    else:
+        print("Generate button not found on screen.")
+    time.sleep(1)
